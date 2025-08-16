3e1a47e0d902f8390fa57b136886b55882e9f93e
Atmois <atmois@allthingslinux.org>
2025-08-16 18:19:57 +0000

    fix the direct ip access

--- configs/00-Core.php
+++ configs/00-Core.php
@@ -35,7 +35,44 @@ $wgMainPageIsDomainRoot = true;
 $wgUseCdn = true;
 
 // https://www.mediawiki.org/wiki/Manual:$wgCdnServersNoPurge
-$wgCdnServersNoPurge = ["2400:cb00::/32", "2606:4700::/32", "2803:f800::/32", "2405:b500::/32", "2405:8100::/32", "2a06:98c0::/29", "2c0f:f248::/32", "173.245.48.0/20", "103.21.244.0/22", "103.22.200.0/22", "103.31.4.0/22", "141.101.64.0/18", "108.162.192.0/18", "190.93.240.0/20", "188.114.96.0/20", "197.234.240.0/22", "198.41.128.0/17", "162.158.0.0/15", "104.16.0.0/13", "104.24.0.0/14", "172.64.0.0/13", "131.0.72.0/22"];
+$wgCdnServersNoPurge = [
+  '10.0.0.2',
+  '173.245.48.0/20',
+  '103.21.244.0/22',
+  '103.22.200.0/22',
+  '103.31.4.0/22',
+  '141.101.64.0/18',
+  '108.162.192.0/18',
+  '190.93.240.0/20',
+  '188.114.96.0/20',
+  '197.234.240.0/22',
+  '198.41.128.0/17',
+  '162.158.0.0/15',
+  '104.16.0.0/13',
+  '104.24.0.0/14',
+  '172.64.0.0/13',
+  '131.0.72.0/22',
+  '2400:cb00::/32',
+  '2606:4700::/32',
+  '2803:f800::/32',
+  '2405:b500::/32',
+  '2405:8100::/32',
+  '2a06:98c0::/29',
+  '2c0f:f248::/32'
+];
+
+$wgCdnServers = [
+	'10.0.0.2',
+];
+
+$wgUsePrivateIPs = true;
+
+// Trust the IP forwarded by the proxy (NPM and Cloudflare)
+if ( isset( $_SERVER['HTTP_X_FORWARDED_FOR'] ) ) {
+    $forwardedIps = explode( ',', $_SERVER['HTTP_X_FORWARDED_FOR'] );
+    // The first IP in the list is the original client IP.
+    $_SERVER['REMOTE_ADDR'] = trim( $forwardedIps[0] );
+}
 
 // https://www.mediawiki.org/wiki/Manual:$wgUsePathInfo
 $wgUsePathInfo = true;
